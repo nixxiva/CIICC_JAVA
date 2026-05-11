0456170b0d4adb1665e65bf352ac06442b2e3b4a
Sarah Angelica Bustinera <sarahbustinera24@gmail.com>
2026-05-11 05:16:53 +0000

    First hello world in codespace

---
+++ Toy.java
@@ -0,0 +1,13 @@
+public class Toy {
+
+    public String name;
+    public String brand;
+    public double price;
+    public int quantity;
+
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+
+
+}
