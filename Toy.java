a72f8787a363c8732140541056cf9a338b80d45d
Sarah Angelica Bustinera <sarahbustinera24@gmail.com>
2026-05-12 04:12:24 +0000

    Instantiation simple

--- Toy.java
+++ Toy.java
@@ -1,12 +1,18 @@
 public class Toy {
 
-    public String name;
+    public static String name = "Gundam";
     public String brand;
     public double price;
     public int quantity;
 
+    void setPrice(double price){
+        this.price = price;
+    }
+
     public static void main(String[] args){
         System.out.println("Hello World");
+        Toy toy1 = new Toy();
+        System.out.println(toy1.name);
     }
 
 
