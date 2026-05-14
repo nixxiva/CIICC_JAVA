29aca77f6562d37b6478dd11583fda636d92f61a
Sarah Angelica Bustinera <sarahbustinera24@gmail.com>
2026-05-14 02:22:48 +0000

    Instantiate in Class

---
+++ chapter1/Main.java
@@ -0,0 +1,34 @@
+package chapter1;
+
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) {
+        // Toy toy1 = new Toy();
+        // A a;
+        // B b;
+        // C c;
+        // Makahiya makahiya;
+        // toy1.brand="";
+        // toy1.name="";
+        // toy1.price=0;
+        // toy1.quantity=12;
+        Random r = new Random();
+        ArrayList p;
+        Scanner o;
+        System.out.println(r.nextInt());
+        // toy1.setPrice(price);
+        System.out.println("Main");
+
+
+    }
+}
+
+//Flight-id
+//Plane-type
+//Cap
+//Crew
+//Passengers
+//Person-id,Name
+//Name-first,Lastname
+//Lastname-id
---
+++ chapter1/Toy.java
@@ -0,0 +1,56 @@
+/**
+ * @author Marco Yimyaem
+ * Toy Object
+ * has a property of name, brand, price, and quantity
+ * whith a method of setPrice() where you can change 
+ * the price state of Toy Object.
+ */
+package chapter1;
+public class Toy {
+    
+    public String name = "Gundam";
+    public String brand;
+    public double price;
+    public int quantity;
+    /* this 
+    * is 
+    * a
+    * multi
+    * line
+    * comment
+    */
+
+
+   /*  set the amount of toy to the argument example setPrice(1000)*/
+    void setPrice(double price){
+        this.price = price;
+    }
+    //psvm - public static void main(String[] args) {}
+    public static void main(String[] args) {
+        //sout System.out.println("hello java trainees");
+        System.out.println("hello java trainees");
+        Toy toy1 = new Toy() ;
+        toy1.brand="Lab Vuvu";
+        toy1.name="Rage Pink";
+        toy1.price=4500;
+        toy1.quantity=12;
+        Toy toy2 = new Toy();
+        toy1.setPrice(toy1.price*0.5);
+        System.out.println(toy1.price);
+    }
+
+}
+class Fruit{}
+class Fruit2{}
+class Fruit3{
+    int numberOfLeaves;
+}
+class Fruit4{
+    public static void main(String[] args) {
+        System.out.println("I am Fruit 4");
+        Fruit3 f3 = new Fruit3();
+        f3.numberOfLeaves=31; 
+        System.out.println(f3.numberOfLeaves);
+        System.out.println(args[3]);
+    }
+}
