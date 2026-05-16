1afc65a3676d7dce962c66ef59d41ae32afb905d
Sarah Angelica Bustinera <sarahbustinera24@gmail.com>
2026-05-16 04:07:29 +0000

    Feat: Added Devcontainer for Java18

---
+++ chapter1/PrimitivesLesson.java
@@ -0,0 +1,10 @@
+package chapter1;
+
+public class PrimitivesLesson {
+    public static void main(String[] args) {
+        byte b = 1; //2^8)
+        System.out.println((Math.powExact(2,8)));
+        System.out.println(Byte.MAX_VALUE);
+
+    }
+}
---
+++ chapter1/ReferenceLesson.java
@@ -0,0 +1,15 @@
+package chapter1;
+
+public class ReferenceLesson {
+    public static void main(String[] args) {
+        
+        Box small = new Box();
+        Box medium = new Box();
+        Box large = new Box();
+    }
+
+}
+
+class Box{
+    int numbers[] = {1,2,3};
+}
