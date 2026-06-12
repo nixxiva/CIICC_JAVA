54cb74e72d20658d557882fac4d8f576b31783d4
Sarah Angelica Bustinera <sarahbustinera24@gmail.com>
2026-06-12 07:58:46 +0000

    Lambda

--- chapter4/Lambda.java
+++ chapter4/Lambda.java
@@ -1,29 +1,84 @@
 package chapter4;
-
+import java.util.*;
+import java.util.function.*;
 public class Lambda {
 
+    static void print(List<Animal> animals,Predicate<Animal> checker){
+        for(Animal animal: animals){
+            if( checker.test(animal)){
+                System.out.print(animal+", ");
+            }
+        }
+        System.out.println("*************");
+    }
+    public static void main(String[] args) {
+        List<Animal> animals = new ArrayList<Animal>();
+        animals.add(new Animal("Fish", false, true,1));
+        animals.add(new Animal("Kangaroo", true, false,5));
+        animals.add(new Animal("Rabbit", true, false,2));
+        animals.add(new Animal("Trutle", false, true,10));
+        animals.add(new Animal("Frog", true, true,2));
+        animals.add(new Animal("Kaola", false, false,6));
+        // print(animals, new CheckIfHopper());
+        // print(animals, new CheckIfSwimmer());
+        print(animals, a->a.canSwim());
+        print(animals, a->a.canHop());
+        print(animals, a->(a.canHop() && a.canSwim()));
+        print(animals, a->(!a.canHop() && !a.canSwim()));
+        print(animals, a->a.getAge()>3);
+        NumberInterface n1 = (a,b,c)-> a*b+100;
+        NumberInterface n2 = (x,y,z)->{return x+y;}; 
+        System.out.println(n1.aPlusB(4, 5,true));
+        System.out.println(n2.aPlusB(8, 9,false));
+        DiscountItems tenPercent = (y)->y-(y*0.10);
+        DiscountItems twentyPercent = (y)->y-(y*0.20);
+        DiscountItems fiftyPercent = (y)->y-(y*0.50);
+        int itemPrince = 1000;
+        System.out.println(tenPercent.dc(itemPrince));
+        System.out.println(twentyPercent.dc(itemPrince));
+        System.out.println(fiftyPercent.dc(itemPrince));
+
+    }
+    
 }
 
 class Animal{
     private String species;
     private boolean canHop;
     private boolean canSwim;
-
-    public Animal(String speciesName, boolean hopper, boolean swimmer){
+    private int age;
+    public Animal(String speciesName,boolean hopper,boolean swimmer,int age){
         species = speciesName;
         canHop = hopper;
         canSwim = swimmer;
+        this.age = age;
     }
 
-    public boolean canHop(){return canHop;}
-    public boolean canSwim(){return canSwim;}
-    public Sring toString(){return species;}
+    public boolean canHop(){ return canHop;}
+    public boolean canSwim(){ return canSwim;}
+    public int getAge(){ return age;}
+    public String toString(){ 
+        return species+" age is ("+age+")";  //Animal@678945BFHJ
+        }
+    }
 
-
+// interface CheckTrait{
+//     boolean test(Animal a);
+// }
+// class CheckIfHopper implements CheckTrait{
+//     public boolean test(Animal a){
+//         return a.canHop();
+//     }
+// }
+// class CheckIfSwimmer implements CheckTrait{
+//     public boolean test(Animal a){
+//         return a.canSwim();
+//     }
+// }
+interface NumberInterface{
+    int aPlusB(int x, int y,boolean z);
 }
 
-
-interface CheckTrait {
-    boolean test(Animal a);
-    
+interface DiscountItems{
+    double dc(double q);
 }
